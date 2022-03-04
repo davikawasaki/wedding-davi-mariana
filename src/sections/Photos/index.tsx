9e0324843827e8316849623eb69f2e0655fe6063
Davi Kawasaki <davishinjik@gmail.com>
2022-03-03 21:10:21 -0300

    feat: our photos

--- src/sections/Photos/index.tsx
+++ src/sections/Photos/index.tsx
@@ -6,64 +6,70 @@ import 'swiper/css'
 import './index.css'
 
 function Photos() {
-  return (
-    <section className="photos w-full relative pt-10 pb-10 block whitespace-normal break-words" id="photos">
-        <h2 className="mb-12 capitalize text-6xl sm:text-9xl text-center">Nossas Fotos</h2>
-        <Swiper
-            className="h-4/5 sm:h-full"
-            breakpoints={{
-                640: {
-                    width: 640,
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                768: {
-                    width: 768,
-                    slidesPerView: 3,
-                    spaceBetween: 25,
-                }
-            }}
-            spaceBetween={0}
-            slidesPerView={1}
-            // onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+    return (
+        <section className="photos w-full relative pt-10 pb-10 block whitespace-normal break-words" id="photos">
+            <h2 className="mb-12 capitalize text-6xl sm:text-9xl text-center">Nossas Fotos</h2>
+            <Swiper
+                className="h-4/5 sm:h-full"
+                breakpoints={{
+                    640: {
+                        width: 640,
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    768: {
+                        width: 768,
+                        slidesPerView: 3,
+                        spaceBetween: 25,
+                    }
+                }}
+                spaceBetween={0}
+                slidesPerView={1}
+                // onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
             >
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://jd.exsythemes.com/img/photo-slider/3-big.jpg" alt="Our wedding" />
-            </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-001.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-002.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-003.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-004.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-005.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-006.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-007.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-008.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-009.jpeg" alt="Our wedding" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/photos/our-photos-010.jpeg" alt="Our wedding" />
+                </SwiperSlide>
 
-            <div className="w-full flex flex-wrap text-center align-center">
-                <div className="w-1/2 pt-5 sm:pt-10">
-                    <SlidePrevButton />
+                <div className="w-full flex flex-wrap text-center align-center">
+                    <div className="w-1/2 pt-5 sm:pt-10">
+                        <SlidePrevButton />
+                    </div>
+                    <div className="w-1/2 pt-5 sm:pt-10">
+                        <SlideNextButton />
+                    </div>
                 </div>
-                <div className="w-1/2 pt-5 sm:pt-10">
-                    <SlideNextButton />
-                </div>
-            </div>
-        </Swiper>
-    </section>
-  );
+            </Swiper>
+        </section>
+    );
 }
 
 export default Photos;
