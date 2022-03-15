2e83b2e90bae54af8ebcf832c1b538ae3b4a839f
Davi Kawasaki <davishinjik@gmail.com>
2022-03-15 00:48:46 -0300

    feat: footer photo

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -8,7 +8,7 @@ function Footer() {
         <div className="flex items-center justify-center w-6/12 top-8">
           <span className="block text-5xl lg:text-7xl relative mr-2 md:mr-10">Mariana</span>
           <div id="footer-image" className="relative hidden md:block">
-            <img className="align-middle w-full border-none" src="https://jd.exsythemes.com/img/footer/photo.jpg" alt="Couple footer" />
+            <img className="align-middle w-full border-none" src="/footer.jpg" alt="Couple footer" />
           </div>
           <span className="block md:hidden text-5xl lg:text-7xl relative ml-2 md:ml-10">e</span>
           <span className="block text-5xl lg:text-7xl relative ml-2 md:ml-10">Davi</span>
