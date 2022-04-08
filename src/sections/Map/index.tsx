d7d8ec24aec83a78ae94ea82dc00c8c7d755b057
Davi Kawasaki <davishinjik@gmail.com>
2022-04-08 08:50:16 -0300

    fix: not transpile map

--- src/sections/Map/index.tsx
+++ src/sections/Map/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import MapGL, { Marker } from "react-map-gl";
 import './index.css';
 
+import mapboxgl from 'mapbox-gl';
+
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 class Map extends React.Component {  
     state = {
       viewport: {
